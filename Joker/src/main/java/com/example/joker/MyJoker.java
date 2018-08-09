b440f286ce3d38df00bec03a5250b8d8f6e4fbd9
Thita Charoentham <thita.charoentham35@qmail.cuny.edu>
2018-08-08 23:25:47 -0400

    Setp1 : crate Java library

---
+++ Joker/src/main/java/com/example/joker/MyJoker.java
@@ -0,0 +1,7 @@
+package com.example.joker;
+
+public class MyJoker {
+    public String getJoke() {
+        return "This is totally a funny joke";
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/JokeActivity.java
@@ -0,0 +1,19 @@
+package com.example.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        String myJoke = getIntent().getStringExtra("joke");
+        Toast.makeText(this, myJoke, Toast.LENGTH_SHORT).show();
+
+
+    }
+}
